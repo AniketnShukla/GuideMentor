1b3cdc62b20a762cbc9c04bb5dfd36528868196b
AniketnShukla <aniketshukla4496@gmail.com>
2024-01-29 14:51:46 +0530

    v3.1.8  commit

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -6,12 +6,22 @@ const __dirname = path.resolve();
 import { promises as fsPromises } from 'fs';
 
 const logEvents = async (message, logFileName) => {
-    const dateTime = `${(new Date(), 'yyyyMMdd\tHH:mm:ss')}`
+    // const dateTime = `${(new Date(), 'yyyyMMdd\tHH:mm:ss')}`
+    const now = new Date();
+    const dateTime = now.toLocaleString('en-US', {
+        year: 'numeric', 
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        
+    })
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`
 
     try{
         if(!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
-            await fsPromises.mkdir(path.join(__dirname, 'backend', 'logs'))
+            await fsPromises.mkdir(path.join(__dirname, 'backend', 'logs'), {recursive: true})
         }
         await fsPromises.appendFile(path.join(__dirname, 'backend', 'logs', logFileName), logItem)
     }catch(err){
